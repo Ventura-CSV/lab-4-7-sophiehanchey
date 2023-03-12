989f3340ea7e902a334a5b91c00c9d289abd16c2
Kyu Lee <kwooler@gmail.com>
2023-03-11 20:59:48 -0800

    L47

--- main_test.py
+++ main_test.py
@@ -27,7 +27,7 @@ def test_main_1():
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '1\n2\n3\n4\n5\n200'
+    datastr = '1\n2\n3\n4\n5\n200\n100'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -38,7 +38,7 @@ def test_main_2():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*215[\w,\W]*'
+    regex_string = r'[\w,\W]*115[\w,\W]*'
     res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
