cfbece5ea488cef82434f80a9f1f992648194969
Kyu Lee <kwooler@gmail.com>
2023-05-23 21:15:36 -0700

    Lab 4-7

--- main_test.py
+++ main_test.py
@@ -7,38 +7,33 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '28\n71\n16'
+    datastr = '5\n4\n3\n2\n1\n5'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
+    assert ret == [5, 4, 3, 2, 1]
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*115[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*115[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
 
 
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '1\n2\n3\n4\n5\n100'
+    datastr = '100\n90\n110'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
-
-    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*115[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    assert ret == [100, 90]
