4f1627de463151a76240d77ed17ee81dfac2c982
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 14:51:30 -0700

    fixed code, pytest passed

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ def main():
     while True:
         currentinput = int(input('Please enter a number: '))
         numbers.append(currentinput)
-        print(numbers[-2])
 
-        if(currentinput>numbers[-2] or len(numbers) < 1):
-            break
-            
+        #checks that the list is long enough to have a previous value
+        if(len(numbers)>1):
+            # print(numbers[-2]) -> checks what the previous value was
+            if(currentinput > numbers[-2]):
+                break
+    
+    #excludes final number
+    del numbers[-1]
     
 
     ########################################
